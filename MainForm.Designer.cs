9c89eeb40c281f1833c916df9f7e01d75e4c7486
AppleFlavored <36705595+AppleFlavored@users.noreply.github.com>
2020-09-28 16:32:53 -0400

    oops

---
+++ MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace MPClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.darkToolStrip1 = new DarkUI.Controls.DarkToolStrip();
+            this.deviceSelection = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnPlay = new System.Windows.Forms.ToolStripButton();
+            this.btnRepeat = new System.Windows.Forms.ToolStripButton();
+            this.btnSelectSong = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1.SuspendLayout();
+            this.darkToolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.DarkMagenta;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 305);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(598, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.statusLabel.Size = new System.Drawing.Size(79, 22);
+            this.statusLabel.Text = "Not Playing";
+            // 
+            // darkToolStrip1
+            // 
+            this.darkToolStrip1.AutoSize = false;
+            this.darkToolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkToolStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkToolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deviceSelection,
+            this.toolStripSeparator1,
+            this.btnPlay,
+            this.btnRepeat,
+            this.btnSelectSong});
+            this.darkToolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.darkToolStrip1.Name = "darkToolStrip1";
+            this.darkToolStrip1.Padding = new System.Windows.Forms.Padding(5, 0, 1, 0);
+            this.darkToolStrip1.Size = new System.Drawing.Size(598, 28);
+            this.darkToolStrip1.TabIndex = 2;
+            this.darkToolStrip1.Text = "darkToolStrip1";
+            // 
+            // deviceSelection
+            // 
+            this.deviceSelection.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.deviceSelection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.deviceSelection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deviceSelection.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.deviceSelection.MaxDropDownItems = 12;
+            this.deviceSelection.Name = "deviceSelection";
+            this.deviceSelection.Size = new System.Drawing.Size(121, 28);
+            this.deviceSelection.SelectedIndexChanged += new System.EventHandler(this.outputDevice_SelectedIndexChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.toolStripSeparator1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.toolStripSeparator1.Margin = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 28);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.btnPlay.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.btnPlay.Image = global::MPClient.Properties.Resources.control_play;
+            this.btnPlay.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(23, 25);
+            this.btnPlay.Text = "toolStripButton1";
+            this.btnPlay.ToolTipText = "Play";
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnRepeat
+            // 
+            this.btnRepeat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.btnRepeat.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnRepeat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.btnRepeat.Image = global::MPClient.Properties.Resources.control_repeat;
+            this.btnRepeat.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRepeat.Name = "btnRepeat";
+            this.btnRepeat.Size = new System.Drawing.Size(23, 25);
+            this.btnRepeat.Text = "toolStripButton2";
+            this.btnRepeat.ToolTipText = "Loop Song";
+            this.btnRepeat.Click += new System.EventHandler(this.btnRepeat_Click);
+            // 
+            // btnSelectSong
+            // 
+            this.btnSelectSong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.btnSelectSong.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSelectSong.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.btnSelectSong.Image = global::MPClient.Properties.Resources.sound;
+            this.btnSelectSong.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSelectSong.Name = "btnSelectSong";
+            this.btnSelectSong.Size = new System.Drawing.Size(24, 24);
+            this.btnSelectSong.Text = "toolStripButton1";
+            this.btnSelectSong.ToolTipText = "Select Song";
+            this.btnSelectSong.Click += new System.EventHandler(this.btnSelectSong_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(598, 327);
+            this.Controls.Add(this.darkToolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "MPClient";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.darkToolStrip1.ResumeLayout(false);
+            this.darkToolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private DarkUI.Controls.DarkToolStrip darkToolStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnPlay;
+        private System.Windows.Forms.ToolStripButton btnRepeat;
+        private System.Windows.Forms.ToolStripComboBox deviceSelection;
+        private System.Windows.Forms.ToolStripButton btnSelectSong;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,170 @@
+﻿using Melanchall.DryWetMidi.Devices;
+using Melanchall.DryWetMidi.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Melanchall.DryWetMidi.Interaction;
+using Timer = System.Windows.Forms.Timer;
+using MPClient.Properties;
+using NAudio.Wave;
+using System.Net.Http.Headers;
+using System.IO;
+
+namespace MPClient
+{
+    public partial class MainForm : Form
+    {
+        private OutputDevice device;
+        private Playback currentPlayback;
+        private Timer playbackTimer;
+        private MidiFile currentFile;
+        private bool shouldLoop = false;
+        private string midiPath = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            playbackTimer = new Timer();
+            playbackTimer.Interval = 1000;
+            playbackTimer.Tick += PlaybackTimer_Tick;
+
+            foreach (OutputDevice device in OutputDevice.GetAll())
+            {
+                deviceSelection.Items.Add(device.Name);
+            }
+
+            device = OutputDevice.GetById(0);
+            deviceSelection.SelectedIndex = 0;
+        }
+
+        private void outputDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (device.Id != deviceSelection.SelectedIndex)
+                device = OutputDevice.GetById(deviceSelection.SelectedIndex);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (midiPath.Length == 0)
+            {
+                MessageBox.Show("Select a midi file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                SelectMidi();
+                return;
+            }
+
+            if (currentPlayback == null || !currentPlayback.IsRunning)
+            {
+                currentFile = MidiFile.Read(midiPath);
+                currentPlayback = currentFile.GetPlayback(device);
+                currentPlayback.Loop = shouldLoop;
+
+                currentPlayback.Started += CurrentPlayback_Started;
+                currentPlayback.Finished += CurrentPlayback_Stopped;
+                currentPlayback.Stopped += CurrentPlayback_Stopped;
+
+                try
+                {
+                    currentPlayback.Start();
+                }
+                catch (MidiDeviceException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                btnPlay.Image = Resources.control_stop;
+            }
+            else
+            {
+                currentPlayback.Stop();
+                device.TurnAllNotesOff();
+            }
+        }
+
+        private delegate void NeedsUpdate();
+
+        private void UpdateComponents()
+        {
+            deviceSelection.Enabled = true;
+            statusLabel.Text = "Not Playing";
+            statusStrip1.BackColor = Color.DarkMagenta;
+            btnPlay.Image = Resources.control_play;
+            btnPlay.ToolTipText = "Play";
+            btnSelectSong.Enabled = true;
+            playbackTimer.Stop();
+        }
+
+        private void CurrentPlayback_Stopped(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+                Invoke(new NeedsUpdate(UpdateComponents));
+            else
+                UpdateComponents();
+        }
+
+        private void CurrentPlayback_Started(object sender, EventArgs e)
+        {
+            deviceSelection.Enabled = false;
+            playbackTimer.Start();
+            btnPlay.ToolTipText = "Stop";
+            statusStrip1.BackColor = Color.DodgerBlue;
+            statusLabel.Text = "Loading";
+            btnSelectSong.Enabled = false;
+        }
+
+        private void PlaybackTimer_Tick(object sender, EventArgs e)
+        {
+            MetricTimeSpan current = currentPlayback.GetCurrentTime<MetricTimeSpan>();
+            MetricTimeSpan duration = currentPlayback.GetDuration<MetricTimeSpan>();
+
+            statusLabel.Text = $"Playing {Path.GetFileName(midiPath)} [{ToTimeString(current)} - {ToTimeString(duration)}]";
+        }
+
+        private string ToTimeString(MetricTimeSpan time)
+        {
+            string seconds = time.Seconds.ToString();
+
+            if (time.Seconds < 10)
+                seconds = seconds.Insert(0, "0");
+
+            return $"{time.Minutes}:{seconds}";
+        }
+
+        private void btnRepeat_Click(object sender, EventArgs e)
+        {
+            shouldLoop = !shouldLoop;
+
+            if (currentPlayback != null)
+                currentPlayback.Loop = shouldLoop;
+
+            if (shouldLoop)
+                btnRepeat.Image = Resources.control_repeat_blue;
+            else
+                btnRepeat.Image = Resources.control_repeat;
+        }
+
+        private void btnSelectSong_Click(object sender, EventArgs e)
+        {
+            SelectMidi();
+        }
+
+        private void SelectMidi()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Midi Files|*.mid";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                midiPath = dialog.FileName;
+            }
+        }
+    }
+}
